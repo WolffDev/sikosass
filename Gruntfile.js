adfc76bb48209368e6283aafd3c373eb7eab2a35
WolffDev <info@davidwolff.dk>
2016-02-02 10:49:25 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'), //tells where the json file is
+
+
+
+    /**
+     * Sass task
+     */
+     sass: {
+       dev: {
+         options: {
+           style: 'expanded',
+           sourcemap: 'none',
+         },
+         files: {
+           'compiled/style-human.css': 'sass/style.scss'
+         }
+       },
+       dist: {
+         options: {
+           style: 'compressed',
+           sourcemap: 'none',
+         },
+         files: {
+           'compiled/style.css': 'sass/style.scss'
+         }
+       }
+     },
+
+     /**
+      * autoprefixer
+      */
+
+     autoprefixer: {
+       options: {
+         browser: ['last 2 versions']
+       },
+       // prefix all files
+       multiple_files: {
+         expand: true,
+         flatten: true,
+         src: 'compiled/*.css',
+         dest: ''
+       }
+     },
+
+    /**
+     * Watch task
+     */
+     watch: {
+       css: {
+         files: '**/*.scss',
+         tasks: ['sass', 'autoprefixer']
+       }
+     }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.registerTask('default',['watch']);
+
+
+}
